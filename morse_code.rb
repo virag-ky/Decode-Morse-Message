176278a3609e876826c6df2e740aaf6325b9fac3
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-22 11:14:16 +0300

    Add method for making character to uppercase

---
+++ morse_code.rb
@@ -0,0 +1,7 @@
+# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+
+def decode_char(string)
+  string.upcase
+end
+
+puts decode_char('maisy')
